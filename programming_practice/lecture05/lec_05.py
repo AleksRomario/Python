5ae2fbde0bf9310a5f1dbf0da5ea259bbf11afa9
Aleksandrs Romanovs <aleks.romario@gmail.com>
2020-11-11 16:55:25 +0300

    Decomposition

--- programming_practice/lecture05/lec_05.py
+++ programming_practice/lecture05/lec_05.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # создаение кода с точки зрения проектирования. 
-
 def draw_house(x, y, width, height):
 	'''
 	Функция рисует домик ширины width и height от пророной точки (x, y),
