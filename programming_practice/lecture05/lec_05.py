a87bbff3b5f765c425b502d3d073695abad53df3
Aleksandrs Romanovs <aleks.romario@gmail.com>
2020-11-11 16:51:01 +0300

    during lecture 5
    made gouse function decompositiom

---
+++ programming_practice/lecture05/lec_05.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# создаение кода с точки зрения проектирования. 
+
+def draw_house(x, y, width, height):
+	'''
+	Функция рисует домик ширины width и height от пророной точки (x, y),
+	которая находится в середине нижней точки фундамента
+	:param x:
+	:param y:
+	:param width:
+	:param height:
+	:return:
+	'''
+	print('переданные данные в функцию', x, y , width , height)
+	pass
+
+
+x, y = 100, 100
+width, height = 100, 100
+
+draw_house(x, y, width, height)
+
+
+
+
